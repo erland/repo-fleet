4569c59c5de01d6ca9eccf899f3b4d5343702231
Erland Lindmark <erland@isaksson.info>
2026-09-18 10:26:00 +0200

    Add GitHub conditional request result

---
+++ backend/src/main/java/info/isaksson/erland/repofleet/github/conditional/GitHubConditionalResult.java
@@ -0,0 +1,24 @@
+package info.isaksson.erland.repofleet.github.conditional;
+
+public record GitHubConditionalResult<T>(
+        Status status,
+        T value,
+        String etag) {
+
+    public enum Status {
+        MODIFIED,
+        NOT_MODIFIED
+    }
+
+    public static <T> GitHubConditionalResult<T> modified(T value, String etag) {
+        return new GitHubConditionalResult<>(Status.MODIFIED, value, etag);
+    }
+
+    public static <T> GitHubConditionalResult<T> notModified(String etag) {
+        return new GitHubConditionalResult<>(Status.NOT_MODIFIED, null, etag);
+    }
+
+    public boolean notModified() {
+        return status == Status.NOT_MODIFIED;
+    }
+}
