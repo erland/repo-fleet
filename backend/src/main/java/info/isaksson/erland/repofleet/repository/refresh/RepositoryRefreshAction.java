f1e7cf407ae3332ad34b893b0b88a4cee86b7c8f
Erland Lindmark <erland@isaksson.info>
2026-09-18 10:17:24 +0200

    Add repository refresh actions

---
+++ backend/src/main/java/info/isaksson/erland/repofleet/repository/refresh/RepositoryRefreshAction.java
@@ -0,0 +1,8 @@
+package info.isaksson.erland.repofleet.repository.refresh;
+
+public enum RepositoryRefreshAction {
+    REUSE_CACHED,
+    REFRESH_SELECTED_CATEGORIES,
+    FULL_ENRICHMENT,
+    FULL_ENRICHMENT_NEW
+}
