19e5ce216f8bf03ad9f8491c904dd27ba11e5255
Erland Lindmark <erland@isaksson.info>
2026-09-18 11:24:25 +0200

    Add repository rule types

---
+++ backend/src/main/java/info/isaksson/erland/repofleet/standards/RepositoryRuleType.java
@@ -0,0 +1,10 @@
+package info.isaksson.erland.repofleet.standards;
+
+public enum RepositoryRuleType {
+    LICENSE_REQUIRED,
+    ACTIONS_WORKFLOW_REQUIRED,
+    PUBLISHED_RELEASE_REQUIRED,
+    REQUIRED_TOPIC,
+    MAXIMUM_INACTIVITY_AGE,
+    README_REQUIRED
+}
