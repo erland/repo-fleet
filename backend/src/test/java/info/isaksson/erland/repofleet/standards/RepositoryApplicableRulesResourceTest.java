e9b0cfc5e2eac2e9ce7a153e1c24717db9b5cfdd
Erland Lindmark <erland@isaksson.info>
2026-09-18 11:36:25 +0200

    Make applicable rules API test deterministic

--- backend/src/test/java/info/isaksson/erland/repofleet/standards/RepositoryApplicableRulesResourceTest.java
+++ backend/src/test/java/info/isaksson/erland/repofleet/standards/RepositoryApplicableRulesResourceTest.java
@@ -1,8 +1,6 @@
 package info.isaksson.erland.repofleet.standards;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.anyOf;
-import static org.hamcrest.Matchers.equalTo;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -15,6 +13,6 @@ class RepositoryApplicableRulesResourceTest {
         given()
                 .when().get("/api/standards/repositories/999999/rules")
                 .then()
-                .statusCode(anyOf(equalTo(404), equalTo(200)));
+                .statusCode(404);
     }
 }
